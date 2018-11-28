5c8b0e36ac59ff533cab1dd1defda7bcb399ac75
shengxiao <2300797269@qq.com>
2018-11-28 15:20:58 +0800

    clear

--- app/src/main/java/com/yunche/android/test/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.yunche.android.test;
-
-/**
- * Created by shengxiao on 2018/11/26.
- */
-
-public class Constants {
-    public static String ANYCHAT_ID = "678fe9c8-fe64-4bee-8020-5c249defc561";
-}
--- facelib/src/main/java/com/yunche/android/yunchevideosdk/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.yunche.android.yunchevideosdk;
-
-/**
- * Created by shengxiao on 2018/11/26.
- */
-
-public class Constants {
-    public static String ANYCHAT_ID = "";
-}
